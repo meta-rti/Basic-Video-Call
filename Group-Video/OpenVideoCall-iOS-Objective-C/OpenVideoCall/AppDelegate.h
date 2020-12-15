93dddab17763a750724d564b706647709e0cb7fa
jiayanan <2396220501@qq.com>
2020-12-15 17:52:06 +0800

    OpenVideoCall-iOS-Objective-C

---
+++ Group-Video/OpenVideoCall-iOS-Objective-C/OpenVideoCall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenVideoCall
+//
+//  Created by 3 on 2020/12/15.
+//  Copyright © 2020年 Wuji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@property (assign, nonatomic) UIInterfaceOrientationMask orientation;
+@end
+
---
+++ Group-Video/OpenVideoCall-iOS-Objective-C/OpenVideoCall/Encryption.h
@@ -0,0 +1,28 @@
+//
+//  EncryptionType.h
+//  OpenVideoCall
+//
+//  Created by 3 on 2020/12/15.
+//  Copyright © 2017 Wuji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WujiRTCFramework_Debug/WujiRTCFramework_Debug.h>
+
+typedef NS_ENUM(int, EncryptionType) {
+    EncryptionTypeXTS128,
+    EncryptionTypeXTS256,
+    EncryptionTypeNone,
+};
+
+@interface Encryption : NSObject
+@property (nonatomic, copy, readonly) NSString *modeString;
+@property (nonatomic, readonly) WujiEncryptionMode modeValue;
+@property (nonatomic, copy, readonly) NSString *description;
+@property (nonatomic, assign) EncryptionType type;
+@property (nonatomic, copy) NSString *secret;
+
++ (NSString *)modeStringWithType:(EncryptionType)type;
++ (NSString *)descriptionWithType:(EncryptionType)type;
++ (NSArray *)allTypesArray;
+@end
