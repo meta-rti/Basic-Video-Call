4f13dd3709d7099f5bca111bd6bb78d0a8c3b928
jiayanan <2396220501@qq.com>
2020-12-15 17:53:05 +0800

    OpenVideoCall-iOS-Objective-C

---
+++ Group-Video/OpenVideoCall-iOS-Objective-C/OpenVideoCall/KeyCenter.h
@@ -0,0 +1,14 @@
+//
+//  KeyCenter.h
+//  OpenVideoCall
+//
+//  Created by GongYuhua on 2016/9/12.
+//  Copyright © 2020年 Wuji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeyCenter : NSObject
++ (NSString *)AppId;
++ (NSString *)Token;
+@end
---
+++ Group-Video/OpenVideoCall-iOS-Objective-C/OpenVideoCall/LastmileViewController.h
@@ -0,0 +1,20 @@
+//
+//  LastmileViewController.h
+//  OpenVideoCall
+//
+//  Created by 3 on 2020/12/15.
+//  Copyright © 2019 Wuji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WujiRTCFramework_Debug/WujiRTCFramework_Debug.h>
+
+@class LastmileViewController;
+@protocol LastmileVCDataSource <NSObject>
+- (WujiRtcEngineKit *)lastmileVCNeedWujiKit;
+
+@end
+
+@interface LastmileViewController : UITableViewController
+@property (weak, nonatomic) id<LastmileVCDataSource> dataSource;
+@end
