0374f82eda4f75ff5800a34a750aa437e32f5feb
jiayanan <2396220501@qq.com>
2020-12-18 11:22:35 +0800

    Wuji-iOS-Tutorial-Swift-1to1

---
+++ One-to-One-Video/Wuji-iOS-Tutorial-Swift-1to1/Wuji-iOS-Tutorial/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Wuji iOS Tutorial
+//
+//  Created by 3 on 2020/12/17.
+//  Copyright © 2020 Wuji. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
