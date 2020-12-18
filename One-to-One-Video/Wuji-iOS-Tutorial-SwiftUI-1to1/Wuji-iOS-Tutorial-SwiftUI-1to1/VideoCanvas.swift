217996c544649bd3fb273a2c85b956f8d8db1418
jiayanan <2396220501@qq.com>
2020-12-18 11:25:23 +0800

    Wuji-iOS-Tutorial-SwiftUI-1to1

---
+++ One-to-One-Video/Wuji-iOS-Tutorial-SwiftUI-1to1/Wuji-iOS-Tutorial-SwiftUI-1to1/VideoCanvas.swift
@@ -0,0 +1,26 @@
+//
+//  VideoView.swift
+//  Wuji-iOS-Tutorial-SwiftUI-1to1
+//
+//  Created by 3 on 2020/12/17.
+//  Copyright © 2020 Wuji. All rights reserved.
+//
+
+import SwiftUI
+
+struct VideoCanvas : UIViewRepresentable {
+    let rendererView = UIView()
+    
+    func makeUIView(context: Context) -> UIView {
+        rendererView.backgroundColor = UIColor.gray
+        return rendererView
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) { }
+}
+
+struct VideoView_Previews : PreviewProvider {
+    static var previews: some View {
+        VideoCanvas()
+    }
+}
