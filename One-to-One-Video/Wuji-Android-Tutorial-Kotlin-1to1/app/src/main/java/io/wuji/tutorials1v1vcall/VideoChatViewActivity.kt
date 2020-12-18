637c1039f45dc4ef24a5d8f51d770ce5a2cfee74
@MyPrivateWeb <1948304571@qq.com>
2020-12-18 12:01:31 +0800

    submit  wuji-Android-Tutorial-Kotlin-1to1   demo

---
+++ One-to-One-Video/Wuji-Android-Tutorial-Kotlin-1to1/app/src/main/java/io/wuji/tutorials1v1vcall/VideoChatViewActivity.kt
@@ -0,0 +1,337 @@
+package io.wuji.tutorials1v1vcall
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceView
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import co.wuji.rtc.IRtcEngineEventHandler
+import co.wuji.rtc.RtcEngine
+import co.wuji.rtc.video.VideoCanvas
+import co.wuji.rtc.video.VideoEncoderConfiguration
+import com.wuji.wuji_tutorial_kotlin_1to1.R
+
+class VideoChatViewActivity : AppCompatActivity() {
+
+    private var mRtcEngine: RtcEngine? = null
+    private val mRtcEventHandler = object : IRtcEngineEventHandler() {
+        /**
+         * Occurs when the first remote video frame is received and decoded.
+         * This callback is triggered in either of the following scenarios:
+         *
+         *     The remote user joins the channel and sends the video stream.
+         *     The remote user stops sending the video stream and re-sends it after 15 seconds. Possible reasons include:
+         *         The remote user leaves channel.
+         *         The remote user drops offline.
+         *         The remote user calls the muteLocalVideoStream method.
+         *         The remote user calls the disableVideo method.
+         *
+         * @param uid User ID of the remote user sending the video streams.
+         * @param width Width (pixels) of the video stream.
+         * @param height Height (pixels) of the video stream.
+         * @param elapsed Time elapsed (ms) from the local user calling the joinChannel method until this callback is triggered.
+         */
+        override fun onFirstRemoteVideoDecoded(uid: Int, width: Int, height: Int, elapsed: Int) {
+            runOnUiThread { setupRemoteVideo(uid) }
+        }
+
+        /**
+         * Occurs when a remote user (Communication)/host (Live Broadcast) leaves the channel.
+         *
+         * There are two reasons for users to become offline:
+         *
+         *     Leave the channel: When the user/host leaves the channel, the user/host sends a
+         *     goodbye message. When this message is received, the SDK determines that the
+         *     user/host leaves the channel.
+         *
+         *     Drop offline: When no data packet of the user or host is received for a certain
+         *     period of time (20 seconds for the communication profile, and more for the live
+         *     broadcast profile), the SDK assumes that the user/host drops offline. A poor
+         *     network connection may lead to false detections, so we recommend using the
+         *     Wuji RTM SDK for reliable offline detection.
+         *
+         * @param uid ID of the user or host who leaves the channel or goes offline.
+         * @param reason Reason why the user goes offline:
+         *
+         *     USER_OFFLINE_QUIT(0): The user left the current channel.
+         *     USER_OFFLINE_DROPPED(1): The SDK timed out and the user dropped offline because no data packet was received within a certain period of time. If a user quits the call and the message is not passed to the SDK (due to an unreliable channel), the SDK assumes the user dropped offline.
+         *     USER_OFFLINE_BECOME_AUDIENCE(2): (Live broadcast only.) The client role switched from the host to the audience.
+         */
+        override fun onUserOffline(uid: Int, reason: Int) {
+            runOnUiThread { onRemoteUserLeft() }
+        }
+
+        /**
+         * Occurs when a remote user stops/resumes sending the video stream.
+         *
+         * @param uid ID of the remote user.
+         * @param muted Whether the remote user's video stream playback pauses/resumes:
+         * true: Pause.
+         * false: Resume.
+         */
+        override fun onUserMuteVideo(uid: Int, muted: Boolean) {
+            runOnUiThread { onRemoteUserVideoMuted(uid, muted) }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_chat_view)
+
+        if (checkSelfPermission(
+                Manifest.permission.RECORD_AUDIO,
+                PERMISSION_REQ_ID_RECORD_AUDIO
+            ) && checkSelfPermission(
+                Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA
+            )
+        ) {
+            initWujiEngineAndJoinChannel()
+        }
+    }
+
+    private fun initWujiEngineAndJoinChannel() {
+        initializeWujiEngine()
+        setupVideoProfile()
+        setupLocalVideo()
+        joinChannel()
+    }
+
+    private fun checkSelfPermission(permission: String, requestCode: Int): Boolean {
+        Log.i(LOG_TAG, "checkSelfPermission $permission $requestCode")
+        if (ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(permission),
+                requestCode
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        Log.i(LOG_TAG, "onRequestPermissionsResult " + grantResults[0] + " " + requestCode)
+
+        when (requestCode) {
+            PERMISSION_REQ_ID_RECORD_AUDIO -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA)
+                } else {
+                    showLongToast("No permission for " + Manifest.permission.RECORD_AUDIO)
+                    finish()
+                }
+            }
+            PERMISSION_REQ_ID_CAMERA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initWujiEngineAndJoinChannel()
+                } else {
+                    showLongToast("No permission for " + Manifest.permission.CAMERA)
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun showLongToast(msg: String) {
+        this.runOnUiThread { Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG).show() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        leaveChannel()
+        /*
+          Destroys the RtcEngine instance and releases all resources used by the Wuji SDK.
+
+          This method is useful for apps that occasionally make voice or video calls,
+          to free up resources for other operations when not making calls.
+         */
+        RtcEngine.destroy()
+        mRtcEngine = null
+    }
+
+    fun onLocalVideoMuteClicked(view: View) {
+        val iv = view as ImageView
+        if (iv.isSelected) {
+            iv.isSelected = false
+            iv.clearColorFilter()
+        } else {
+            iv.isSelected = true
+            iv.setColorFilter(resources.getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY)
+        }
+
+        // Stops/Resumes sending the local video stream.
+        mRtcEngine!!.muteLocalVideoStream(iv.isSelected)
+
+        val container = findViewById(R.id.local_video_view_container) as FrameLayout
+        val surfaceView = container.getChildAt(0) as SurfaceView
+        surfaceView.setZOrderMediaOverlay(!iv.isSelected)
+        surfaceView.visibility = if (iv.isSelected) View.GONE else View.VISIBLE
+    }
+
+    fun onLocalAudioMuteClicked(view: View) {
+        val iv = view as ImageView
+        if (iv.isSelected) {
+            iv.isSelected = false
+            iv.clearColorFilter()
+        } else {
+            iv.isSelected = true
+            iv.setColorFilter(resources.getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY)
+        }
+
+        // Stops/Resumes sending the local audio stream.
+        mRtcEngine!!.muteLocalAudioStream(iv.isSelected)
+    }
+
+    fun onSwitchCameraClicked(view: View) {
+        // Switches between front and rear cameras.
+        mRtcEngine!!.switchCamera()
+    }
+
+    fun onEncCallClicked(view: View) {
+        finish()
+    }
+
+    private fun initializeWujiEngine() {
+        try {
+            mRtcEngine =
+                RtcEngine.create(baseContext, getString(R.string.wuji_app_id), mRtcEventHandler)
+        } catch (e: Exception) {
+            Log.e(LOG_TAG, Log.getStackTraceString(e))
+
+            throw RuntimeException(
+                "NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(
+                    e
+                )
+            )
+        }
+    }
+
+    private fun setupVideoProfile() {
+        // In simple use cases, we only need to enable video capturing
+        // and rendering once at the initialization step.
+        // Note: audio recording and playing is enabled by default.
+        mRtcEngine!!.enableVideo()
+//      mRtcEngine!!.setVideoProfile(Constants.VIDEO_PROFILE_360P, false) // Earlier than 2.3.0
+
+        // Please go to this page for detailed explanation
+        // https://docs.wuji.io/en/Video/API%20Reference/java/classio_1_1wuji_1_1rtc_1_1_rtc_engine.html#af5f4de754e2c1f493096641c5c5c1d8f
+        mRtcEngine!!.setVideoEncoderConfiguration(
+            VideoEncoderConfiguration(
+                VideoEncoderConfiguration.VD_640x360,
+                VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_15,
+                VideoEncoderConfiguration.STANDARD_BITRATE,
+                VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_FIXED_PORTRAIT
+            )
+        )
+    }
+
+    private fun setupLocalVideo() {
+        // This is used to set a local preview.
+        // The steps setting local and remote view are very similar.
+        // But note that if the local user do not have a uid or do
+        // not care what the uid is, he can set his uid as ZERO.
+        // Our server will assign one and return the uid via the event
+        // handler callback function (onJoinChannelSuccess) after
+        // joining the channel successfully.
+        val container = findViewById(R.id.local_video_view_container) as FrameLayout
+        val surfaceView = RtcEngine.CreateRendererView(baseContext)
+        surfaceView.setZOrderMediaOverlay(true)
+        container.addView(surfaceView)
+        // Initializes the local video view.
+        // RENDER_MODE_FIT: Uniformly scale the video until one of its dimension fits the boundary. Areas that are not filled due to the disparity in the aspect ratio are filled with black.
+        mRtcEngine!!.setupLocalVideo(VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, 0))
+    }
+
+    private fun joinChannel() {
+        // 1. Users can only see each other after they join the
+        // same channel successfully using the same app id.
+        // 2. One token is only valid for the channel name that
+        // you use to generate this token.
+        var token: String? = getString(R.string.wuji_access_token)
+        if (token!!.isEmpty()) {
+            token = null
+        }
+        mRtcEngine!!.joinChannel(
+            token,
+            "123",
+            "Extra Optional Data",
+            0
+        ) // if you do not specify the uid, we will generate the uid for you
+    }
+
+    private fun setupRemoteVideo(uid: Int) {
+        // Only one remote video view is available for this
+        // tutorial. Here we check if there exists a surface
+        // view tagged as this uid.
+        val container = findViewById(R.id.remote_video_view_container) as FrameLayout
+
+        if (container.childCount >= 1) {
+            return
+        }
+
+        /*
+          Creates the video renderer view.
+          CreateRendererView returns the SurfaceView type. The operation and layout of the view
+          are managed by the app, and the Wuji SDK renders the view provided by the app.
+          The video display view must be created using this method instead of directly
+          calling SurfaceView.
+         */
+        val surfaceView = RtcEngine.CreateRendererView(baseContext)
+        container.addView(surfaceView)
+        // Initializes the video view of a remote user.
+        mRtcEngine!!.setupRemoteVideo(VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, uid))
+
+        surfaceView.tag = uid // for mark purpose
+        val tipMsg = findViewById<TextView>(R.id.quick_tips_when_use_wuji_sdk) // optional UI
+        tipMsg.visibility = View.GONE
+    }
+
+    private fun leaveChannel() {
+        mRtcEngine!!.leaveChannel()
+    }
+
+    private fun onRemoteUserLeft() {
+        val container = findViewById(R.id.remote_video_view_container) as FrameLayout
+        container.removeAllViews()
+
+        val tipMsg = findViewById<TextView>(R.id.quick_tips_when_use_wuji_sdk) // optional UI
+        tipMsg.visibility = View.VISIBLE
+    }
+
+    private fun onRemoteUserVideoMuted(uid: Int, muted: Boolean) {
+        val container = findViewById(R.id.remote_video_view_container) as FrameLayout
+
+        val surfaceView = container.getChildAt(0) as SurfaceView
+
+        val tag = surfaceView.tag
+        if (tag != null && tag as Int == uid) {
+            surfaceView.visibility = if (muted) View.GONE else View.VISIBLE
+        }
+    }
+
+    companion object {
+
+        private val LOG_TAG = VideoChatViewActivity::class.java.simpleName
+
+        private const val PERMISSION_REQ_ID_RECORD_AUDIO = 22
+        private const val PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1
+    }
+}
