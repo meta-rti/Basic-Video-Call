89823b724e1cb45989f62b45c25d2e6e23296b65
jiayanan <2396220501@qq.com>
2021-05-11 11:08:52 +0800

    更换Mac的framework

---
+++ Group-Video/OpenVideoCall-macOS/OpenVideoCall/WujiRTCFramework.framework/Versions/A/Headers/WujiRTCFramework.h
@@ -0,0 +1,23 @@
+//
+//  WujiRTCFramework.h
+//  WujiRTCFramework
+//
+//  Created by anita on 2020/9/2.
+//  Copyright © 2020 liuming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WujiRTCFramework.
+FOUNDATION_EXPORT double WujiRTCFrameworkVersionNumber;
+
+//! Project version string for WujiRTCFramework.
+FOUNDATION_EXPORT const unsigned char WujiRTCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WujiRTCFramework/PublicHeader.h>
+
+#import "WujiRtcEngineKit.h"
+#import "WujiRtmKit.h"
+#import "WujiLiveKit.h"
+#import "WujiRtcChannel.h"
+
