1d28f8154b2b2c6879f2ac24ae16e2e13c9e6084
jiayanan <2396220501@qq.com>
2021-07-13 11:38:14 +0800

    修改注释

--- Group-Video/OpenVideoCall-iOS/OpenVideoCall/KeyCenter.swift
+++ Group-Video/OpenVideoCall-iOS/OpenVideoCall/KeyCenter.swift
@@ -8,7 +8,7 @@
 
 
 struct KeyCenter {
-    static let AppId: String =  <#Your App Id#>
+    static let AppId: String =  "123"
     // assign token to nil if you have not enabled app certificate
-    static var Token: String? = <#Temp Access Token#>
+    static var Token: String? = ""
 }
