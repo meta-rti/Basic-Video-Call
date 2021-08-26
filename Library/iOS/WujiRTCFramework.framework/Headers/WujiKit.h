a6d5533afb749e816ebfb2d00df4df8112906852
jiayanan <2396220501@qq.com>
2021-08-26 19:30:24 +0800

    add Library

---
+++ Library/iOS/WujiRTCFramework.framework/Headers/WujiKit.h
@@ -0,0 +1,15 @@
+//
+//  WujiKit.h
+//  WujiRTCFramework
+//
+//  Created by anita on 2020/9/12.
+//  Copyright © 2020 liuming. All rights reserved.
+//
+
+#ifndef WujiKit_h
+#define WujiKit_h
+#import "WujiRtcEngineKit.h"
+#import "WujiRtmKit.h"
+#import "WujiLiveKit.h"
+#import "WujiRtcChannel.h"
+#endif /* WujiKit_h */
---
+++ Library/iOS/WujiRTCFramework.framework/Headers/WujiRTCFramework.h
@@ -0,0 +1,21 @@
+//
+//  WujiRTCFramework.h
+//  WujiRTCFramework
+//
+//  Created by anita on 2020/8/31.
+//  Copyright © 2020 liuming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WujiRTCFramework.
+FOUNDATION_EXPORT double WujiRTCFrameworkVersionNumber;
+
+//! Project version string for WujiRTCFramework.
+FOUNDATION_EXPORT const unsigned char WujiRTCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WujiRTCFramework/PublicHeader.h>
+
+#import <WujiRTCFramework/WujiKit.h>
+
+
