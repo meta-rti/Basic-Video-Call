6b6b1f71c4c5622b706235045a63e9d5151470c8
liuming <liuming@wuji.co>
2022-01-18 13:40:11 +0800

    update frameowork to 2.0

---
+++ Library/Mac/MetaRTCFramework.framework/Versions/A/Headers/MetaRTCFramework.h
@@ -0,0 +1,21 @@
+//
+//  MetaRTCFramework.h
+//  MetaRTCFramework
+//
+//  Created by yoyo on 2022/1/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MetaRTCFramework.
+FOUNDATION_EXPORT double MetaRTCFrameworkVersionNumber;
+
+//! Project version string for MetaRTCFramework.
+FOUNDATION_EXPORT const unsigned char MetaRTCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetaRTCFramework/PublicHeader.h>
+
+#import "MetaRtcEngineKit.h"
+#import "MetaRtmKit.h"
+#import "MetaLiveKit.h"
+#import "MetaRtcChannel.h"
